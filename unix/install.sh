9ab99f55048aec872aafb39f4e87660500496a14
Jon Koenig <jkoenig@outlook.com>
2021-11-10 18:47:23 -0700

    Updated unix installation script.

--- unix/install.sh
+++ unix/install.sh
@@ -1,11 +1,38 @@
 #!/bin/sh
 
-SCRIPT=$(realpath $0)
-SCRIPTDIR=$(dirname $SCRIPT)
+SCRIPT_PATH=${BASH_SOURCE[0]}
+SCRIPT_NAME=${PROG_PATH##*/}
+SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
 
-# Install ZSH
+ZSH_CONTROL=0
+SSH_CONTROL=0
+VIM_CONTROL=0
+
+#
+# Zsh
+#
 if test -f "${HOME}/.zshrc"; then
-  echo ".zshrc exists."
+  echo "~/.zshrc already exists."
 else
-  ln -s "${SCRIPTDIR}/zsh/zshrc" "${HOME}/.zshrc"
+  ln -s "${SCRIPT_DIR}/zsh/zshrc" "${HOME}/.zshrc"
 fi
+
+#
+# SSH
+#
+if test -f "${HOME}/.ssh/config"; then
+  echo "~/.ssh/config already exists."
+else
+  ln -s "${SCRIPT_DIR}/ssh/config" "${HOME}/.ssh/config"
+fi
+
+#
+# Vim 
+#
+if test -f "${HOME}/.vimrc"; then
+  echo "~/.vimrc already exists."
+else
+  ln -s "${SCRIPT_DIR}/vim/vimrc" "${HOME}/.vimrc"
+fi
+
+source "${HOME}/.zshrc"
