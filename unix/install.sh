7d5b73a7b232b301a6c3bcffbe7a927e9c022307
Jon Koenig <jmkoeni@icloud.com>
2022-08-10 10:14:17 -0600

    Removed some unused stuff from zsh configuration and trimmed the installation script down.

--- unix/install.sh
+++ unix/install.sh
@@ -1,46 +1,8 @@
 #!/bin/sh
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# Show app switcher on all displays by default.
-if [[ $OSTYPE == 'darwin'* ]]; then
-  defaults write com.apple.Dock appswitcher-all-displays -bool true
-  killall Dock
-  echo "Installed macOS tweaks."
-fi
-
-SCRIPT_PATH=${BASH_SOURCE[0]}
-SCRIPT_NAME=${PROG_PATH##*/}
-SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH:-$PWD}")" 2>/dev/null 1>&2 && pwd)"
-
-ZSH_CONTROL=0
-SSH_CONTROL=0
-VIM_CONTROL=0
-
-#
-# Zsh
-#
-if test -f "${HOME}/.zshrc"; then
-  echo "${HOME}/.zshrc already exists, skipped."
-else
-  ln -s "${SCRIPT_DIR}/zsh/zshrc" "${HOME}/.zshrc"
-fi
-
-#
-# SSH
-#
-mkdir ${HOME}/.ssh &> /dev/null
-if test -f "${HOME}/.ssh/config"; then
-  echo "${HOME}/.ssh/config already exists, skipped."
-else
-  ln -s "${SCRIPT_DIR}/ssh/config" "${HOME}/.ssh/config"
-fi
-
-#
-# Vim 
-#
-if test -f "${HOME}/.vimrc"; then
-  echo "${HOME}.vimrc already exists, skipped."
-else
-  ln -s "${SCRIPT_DIR}/vim/vimrc" "${HOME}/.vimrc"
-fi
+ln -s "${SCRIPT_DIR}/zsh/zshrc" "${HOME}/.zshrc"
+ln -s "${SCRIPT_DIR}/ssh/config" "${HOME}/.ssh/config"
+ln -s "${SCRIPT_DIR}/vim/vimrc" "${HOME}/.vimrc"
 
 source "${HOME}/.zshrc"
