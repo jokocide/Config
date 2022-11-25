27d5a3b239a27114e851d112e00e98a0377d184d
Jon Koenig <jon@Jons-MacBook-Air.local>
2022-11-25 07:12:31 -0700

    Updated install script

--- unix/install.sh
+++ unix/install.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-ln -s "${SCRIPT_DIR}/zsh/zshrc" "${HOME}/.zshrc"
-ln -s "${SCRIPT_DIR}/ssh/config" "${HOME}/.ssh/config"
-ln -s "${SCRIPT_DIR}/vim/vimrc" "${HOME}/.vimrc"
+zsh="${HOME}/.zshrc"
+if [ ! -f $zsh ]
+then
+  ln -s "${SCRIPT_DIR}/zsh/zshrc" $zsh
+  source $zsh
+fi
 
-source "${HOME}/.zshrc"
+ssh="${HOME}/.ssh/config"
+if [ ! -f $ssh ]
+then
+  ln -s "${SCRIPT_DIR}/ssh/config" $ssh
+fi
+
+vim="${HOME}/.vimrc"
+if [ ! -f $vim ]
+then
+  ln -s "${SCRIPT_DIR}/vim/vimrc" $vim
+fi
+
+bin="${HOME}/.bin"
+if [ ! -d $bin ]  
+then
+  mkdir $bin
+fi
